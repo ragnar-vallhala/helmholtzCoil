f349d0d4fa6fa0ed1ababd05a4dfe89bd5723d77
Ashu <ashutoshvishwakarma208@gmail.com>
2023-04-26 18:44:11 +0530

    Upd

--- Assets/Script/Probe.cs
+++ Assets/Script/Probe.cs
@@ -23,5 +23,17 @@ public class Probe : MonoBehaviour
         }
         Vector3 vec = new Vector3(0, 0, transform.position.z + step);
         transform.position = vec;
+
+        Coil[] obj = GameObject.FindObjectsOfType<Coil>();
+        Coil coil1 = obj[0];
+        Coil coil2 = obj[1];
+
+        Vector3 r1 = coil1.transform.position - transform.position;
+
+        double i1 = coil1.getCurrent();
+
+        double magField1 = 3.14 * 2 * coil1.getCurrent() * coil1.getRadius() * coil1.getRadius() / Mathf.Pow((r1.sqrMagnitude+ coil1.getCurrent() * coil1.getRadius()),1.5f);
+
+        Debug.Log(magField1);
     }
 }
